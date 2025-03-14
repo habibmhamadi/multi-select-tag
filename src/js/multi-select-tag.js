d9d2e3c65afd8832080dbace071d6a1020d6f48b
habibmhamadi <habibmhamadi@gmail.com>
2025-03-14 16:22:54 +0430

    v4: Major update.

--- src/js/multi-select-tag.js
+++ src/js/multi-select-tag.js
@@ -1,298 +1,262 @@
 // Author: Habib Mhamadi
 // Email: habibmhamadi@gmail.com
 
-
-function MultiSelectTag(el, customs = { shadow: false, rounded: true }) {
-    // Initialize variables
-    var element = null,
-        options = null,
-        customSelectContainer = null,
-        wrapper = null,
-        btnContainer = null,
-        body = null,
-        inputContainer = null,
-        inputBody = null,
-        input = null,
-        button = null,
-        drawer = null,
-        ul = null;
-
-    // Customize tag colors
-    var tagColor = customs.tagColor || {};
-    tagColor.textColor = tagColor.textColor || "#FF5D29";
-    tagColor.borderColor = tagColor.borderColor || "#FF5D29";
-    tagColor.bgColor = tagColor.bgColor || "#FFE9E2";
-
-    // Initialize DOM Parser
-    var domParser = new DOMParser();
-
-    // Initialize
-    init();
-
+function MultiSelectTag(selectElOrId, config) {
+    // Private variables
+    var selectElement,
+        optionsData = [],
+        container,
+        onChange = config.onChange || function() {},
+        required = config.required || false,
+        maxSelection = typeof config.maxSelection === 'number' ? config.maxSelection : Infinity,
+        placeholder = config.placeholder || 'Search',
+        selectedTags = [],
+        filteredOptions = [],
+        highlightedIndex = -1,
+        selectedTagsContainer,
+        tagInput,
+        dropdown;
+  
+    // Resolve the select element from a string id.
+    selectElement = document.getElementById(selectElOrId);
+    if (!selectElement) {
+      throw new Error("Select element not found.");
+    }
+    if (selectElement.tagName !== 'SELECT') {
+      throw new Error("Element is not a select element.");
+    }
+  
+    // Hide the original select element.
+    selectElement.style.display = 'none';
+  
+    // Read options from the select element.
+    for (var i = 0; i < selectElement.options.length; i++) {
+      var option = selectElement.options[i];
+      optionsData.push({
+        id: option.value,
+        label: option.text,
+        preselected: option.selected
+      });
+    }
+  
+    // Create a container for the widget and insert it after the select.
+    container = document.createElement('div');
+    container.className = 'multi-select-tag';
+    selectElement.parentNode.insertBefore(container, selectElement.nextSibling);
+  
+    // Preselect any options marked as selected.
+    for (var j = 0; j < optionsData.length; j++) {
+      if (optionsData[j].preselected) {
+        selectedTags.push({ id: optionsData[j].id, label: optionsData[j].label });
+      }
+    }
+    filteredOptions = optionsData.slice();
+  
+    // Private function: Build the widget's HTML structure.
     function init() {
-        // DOM element initialization
-        element = document.getElementById(el);
-        createElements();
-        initOptions();
-        enableItemSelection();
-        setValues(false);
-
-        // Event listeners
-        button.addEventListener('click', () => {
-            if (drawer.classList.contains('hidden')) {
-                initOptions();
-                enableItemSelection();
-                drawer.classList.remove('hidden');
-                input.focus();
-            } else {
-                drawer.classList.add('hidden');
-            }
-        });
-
-        input.addEventListener('keyup', (e) => {
-            initOptions(e.target.value);
-            enableItemSelection();
-        });
-
-        input.addEventListener('keydown', (e) => {
-            if (e.key === 'Backspace' && !e.target.value && inputContainer.childElementCount > 1) {
-                const child = body.children[inputContainer.childElementCount - 2].firstChild;
-                const option = options.find((op) => op.value == child.dataset.value);
-                option.selected = false;
-                removeTag(child.dataset.value);
-                setValues();
-            }
-        });
-
-        window.addEventListener('click', (e) => {
-            if (!customSelectContainer.contains(e.target)) {
-                if ((e.target.nodeName !== "LI") && (e.target.getAttribute("class") !== "input_checkbox")) {
-                    // hide the list option only if we click outside of it
-                    drawer.classList.add('hidden');
-                } else {
-                    // enable again the click on the list options
-                    enableItemSelection();
-                }
-            }
-        });
+      container.innerHTML = `
+        <div class="wrapper">
+          <div id="selected-tags" class="tag-container">
+            <input type="text" id="tag-input" placeholder="${placeholder}" class="tag-input" autocomplete="off">
+          </div>
+          <ul id="dropdown" class="dropdown hidden"></ul>
+        </div>`;
+      selectedTagsContainer = container.querySelector('#selected-tags');
+      tagInput = container.querySelector('#tag-input');
+      dropdown = container.querySelector('#dropdown');
+      bindEvents();
     }
-
-    function createElements() {
-        // Create custom elements
-        options = getOptions();
-        element.classList.add('hidden');
-
-        // .multi-select-tag
-        customSelectContainer = document.createElement('div');
-        customSelectContainer.classList.add('mult-select-tag');
-
-        // .container
-        wrapper = document.createElement('div');
-        wrapper.classList.add('wrapper');
-
-        // body
-        body = document.createElement('div');
-        body.classList.add('body');
-        if (customs.shadow) {
-            body.classList.add('shadow');
-        }
-        if (customs.rounded) {
-            body.classList.add('rounded');
-        }
-
-        // .input-container
-        inputContainer = document.createElement('div');
-        inputContainer.classList.add('input-container');
-
-        // input
-        input = document.createElement('input');
-        input.classList.add('input');
-        input.placeholder = `${customs.placeholder || 'Search...'}`;
-
-        inputBody = document.createElement('inputBody');
-        inputBody.classList.add('input-body');
-        inputBody.append(input);
-
-        body.append(inputContainer);
-
-        // .btn-container
-        btnContainer = document.createElement('div');
-        btnContainer.classList.add('btn-container');
-
-        // button
-        button = document.createElement('button');
-        button.type = 'button';
-        btnContainer.append(button);
-
-        const icon = domParser.parseFromString(
-            `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <polyline points="18 15 12 21 6 15"></polyline>
-            </svg>`, 'image/svg+xml').documentElement;
-
-        button.append(icon);
-
-        body.append(btnContainer);
-        wrapper.append(body);
-
-        drawer = document.createElement('div');
-        drawer.classList.add(...['drawer', 'hidden']);
-        if (customs.shadow) {
-            drawer.classList.add('shadow');
-        }
-        if (customs.rounded) {
-            drawer.classList.add('rounded');
-        }
-        drawer.append(inputBody);
-        ul = document.createElement('ul');
-
-        drawer.appendChild(ul);
-
-        customSelectContainer.appendChild(wrapper);
-        customSelectContainer.appendChild(drawer);
-
-        // Place TailwindTagSelection after the element
-        if (element.nextSibling) {
-            element.parentNode.insertBefore(customSelectContainer, element.nextSibling);
-        } else {
-            element.parentNode.appendChild(customSelectContainer);
-        }
-    }
-
-    function createElementInSelectList(option, val, selected = false) {
-        // Create a <li> elmt in the drop-down list,
-        // selected parameters tells if the checkbox need to be selected and the bg color changed
-        const li = document.createElement('li');
-        li.innerHTML = "<input type='checkbox' style='margin:0 0.5em 0 0' class='input_checkbox'>"; // add the checkbox at the left of the <li>
-        li.innerHTML += option.label;
-        li.dataset.value = option.value;
-        const checkbox = li.firstChild;
-        checkbox.dataset.value = option.value;
-
-        // For search
-        if (val && option.label.toLowerCase().startsWith(val.toLowerCase())) {
-            ul.appendChild(li);
-        } else if (!val) {
-            ul.appendChild(li);
-        }
-
-        // Change bg color and checking the checkbox
-        if (selected) {
-            li.style.backgroundColor = tagColor.bgColor;
-            checkbox.checked = true;
-        }
-    }
-
-    function initOptions(val = null) {
-        ul.innerHTML = '';
-        for (var option of options) {
-            // if option already selected
-            if (option.selected) {
-                !isTagSelected(option.value) && createTag(option);
-
-                // We create a option in the list, but with different color
-                createElementInSelectList(option, val, true);
-            } else {
-                createElementInSelectList(option, val);
-            }
-        }
-    }
-
-    function createTag(option) {
-        // Create and show selected item as tag
-        const itemDiv = document.createElement('div');
-        itemDiv.classList.add('item-container');
-        itemDiv.style.color = tagColor.textColor || '#2c7a7b';
-        itemDiv.style.borderColor = tagColor.borderColor || '#81e6d9';
-        itemDiv.style.background = tagColor.bgColor || '#e6fffa';
-        const itemLabel = document.createElement('div');
-        itemLabel.classList.add('item-label');
-        itemLabel.style.color = tagColor.textColor || '#2c7a7b';
-        itemLabel.innerHTML = option.label;
-        itemLabel.dataset.value = option.value;
-        const itemClose = domParser.parseFromString(
-            `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="item-close-svg">
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
-            </svg>`, 'image/svg+xml').documentElement;
-
-        itemClose.addEventListener('click', (e) => {
-            const unselectOption = options.find((op) => op.value == option.value);
-            unselectOption.selected = false;
-            removeTag(option.value);
-            initOptions();
-            setValues();
+  
+    // Private function: Bind event listeners.
+    function bindEvents() {
+      tagInput.addEventListener('input', function(e) {
+        var searchTerm = e.target.value.toLowerCase();
+        filteredOptions = optionsData.filter(function(opt) {
+          return opt.label.toLowerCase().includes(searchTerm);
         });
-
-        itemDiv.appendChild(itemLabel);
-        itemDiv.appendChild(itemClose);
-        inputContainer.append(itemDiv);
-    }
-
-    function enableItemSelection() {
-        // Add click listener to the list items
-        for (var li of ul.children) {
-            li.addEventListener('click', (e) => {
-                if (options.find((o) => o.value == e.target.dataset.value).selected === false) {
-                    // if the option is not selected, we select it
-                    options.find((o) => o.value == e.target.dataset.value).selected = true;
-                    input.value = null;
-                    initOptions();
-                    setValues();
-                    //input.focus() // brings up the list to the input
-                } else {
-                    // if it's already selected, we deselect it
-                    options.find((o) => o.value == e.target.dataset.value).selected = false;
-                    input.value = null;
-                    initOptions();
-                    setValues();
-                    //input.focus() // brings up the list on the input
-                    removeTag(e.target.dataset.value);
-                }
+        highlightedIndex = -1;
+        renderDropdown();
+      });
+  
+      tagInput.addEventListener('keydown', function(e) {
+        var visibleOptions = dropdown.querySelectorAll('li');
+        if (e.key === 'Backspace' && tagInput.value === '') {
+          if (selectedTags.length > 0) {
+            selectedTags.pop();
+            renderSelectedTags();
+            renderDropdown();
+            syncToSelect();
+            onChange(selectedTags);
+            e.preventDefault();
+            return;
+          }
+        }
+        if (e.key === 'ArrowDown') {
+          e.preventDefault();
+          if (visibleOptions.length === 0) return;
+          highlightedIndex = (highlightedIndex + 1) % visibleOptions.length;
+          renderDropdown();
+        } else if (e.key === 'ArrowUp') {
+          e.preventDefault();
+          if (visibleOptions.length === 0) return;
+          highlightedIndex = (highlightedIndex - 1 + visibleOptions.length) % visibleOptions.length;
+          renderDropdown();
+        } else if (e.key === 'Enter') {
+          e.preventDefault();
+          if (highlightedIndex > -1 && visibleOptions[highlightedIndex]) {
+            var selectedLabel = visibleOptions[highlightedIndex].textContent;
+            var option = optionsData.find(function(opt) {
+              return opt.label === selectedLabel;
             });
-        }
-    }
-
-    function isTagSelected(val) {
-        // If the item is already selected
-        for (var child of inputContainer.children) {
-            if (!child.classList.contains('input-body') && child.firstChild.dataset.value == val) {
-                return true;
+            if (option) {
+              selectTag(option);
             }
+          }
         }
-        return false;
+      });
+  
+      document.addEventListener('click', function(e) {
+        if (!container.contains(e.target)) {
+          highlightedIndex = -1;
+          dropdown.classList.add('hidden');
+        }
+      });
+  
+      tagInput.addEventListener('focus', function() {
+        renderDropdown();
+      });
     }
-
-    function removeTag(val) {
-        // Remove selected item
-        for (var child of inputContainer.children) {
-            if (!child.classList.contains('input-body') && child.firstChild.dataset.value == val) {
-                inputContainer.removeChild(child);
-            }
-        }
-    }
-
-    function setValues(fireEvent = true) {
-        // Update element final values
-        selected_values = [];
-        for (var i = 0; i < options.length; i++) {
-            element.options[i].selected = options[i].selected;
-            if (options[i].selected) {
-                selected_values.push({ label: options[i].label, value: options[i].value });
-            }
-        }
-        if (fireEvent && customs.hasOwnProperty('onChange')) {
-            customs.onChange(selected_values);
-        }
-    }
-
-    function getOptions() {
-        // Map element options
-        return [...element.options].map((op) => {
-            return {
-                value: op.value,
-                label: op.label,
-                selected: op.selected,
-            };
+  
+    // Private function: Render the dropdown list, excluding already selected options.
+    function renderDropdown() {
+      dropdown.innerHTML = '';
+      var visibleOptions = filteredOptions.filter(function(opt) {
+        return !selectedTags.find(function(tag) {
+          return tag.id === opt.id;
         });
+      });
+      if (visibleOptions.length === 0) {
+        dropdown.classList.add('hidden');
+        return;
+      }
+      visibleOptions.forEach(function(option, index) {
+        var li = document.createElement('li');
+        li.textContent = option.label;
+        li.className = 'li';
+        if (index === highlightedIndex) {
+          li.classList.add('li-arrow');
+        }
+        li.addEventListener('click', function() {
+          selectTag(option);
+        });
+        dropdown.appendChild(li);
+      });
+      dropdown.classList.remove('hidden');
+      if (highlightedIndex > -1) {
+        var highlightedItem = dropdown.children[highlightedIndex];
+        if (highlightedItem) {
+          highlightedItem.scrollIntoView({ block: 'nearest' });
+        }
+      }
     }
-}
+  
+    // Private function: Render the selected tags.
+    function renderSelectedTags() {
+      var tagItems = selectedTagsContainer.querySelectorAll('.tag-item');
+      for (var k = 0; k < tagItems.length; k++) {
+        tagItems[k].remove();
+      }
+      selectedTags.forEach(function(tag) {
+        var span = document.createElement('span');
+        span.className = 'tag-item';
+        span.textContent = tag.label;
+        var closeBtn = document.createElement('span');
+        closeBtn.className = 'cross';
+        closeBtn.innerHTML = '&times;';
+        closeBtn.addEventListener('click', function() {
+          deselectTag(tag);
+        });
+        span.appendChild(closeBtn);
+        selectedTagsContainer.insertBefore(span, tagInput);
+      });
+    }
+  
+    // Private function: Add a tag to the selection.
+    function selectTag(option) {
+      if (selectedTags.length >= maxSelection) return;
+      if (!selectedTags.find(function(tag) { return tag.id === option.id; })) {
+        selectedTags.push({ id: option.id, label: option.label });
+      }
+      tagInput.value = '';
+      filteredOptions = optionsData.filter(function(opt) {
+        return opt.label.toLowerCase().includes(tagInput.value.toLowerCase());
+      });
+      highlightedIndex = -1;
+      renderSelectedTags();
+      renderDropdown();
+      syncToSelect();
+      onChange(selectedTags);
+    }
+  
+    // Private function: Remove a tag from the selection.
+    function deselectTag(tag) {
+      selectedTags = selectedTags.filter(function(t) {
+        return t.id !== tag.id;
+      });
+      renderSelectedTags();
+      renderDropdown();
+      syncToSelect();
+      onChange(selectedTags);
+    }
+  
+    // Private function: Synchronize the widget's selection with the underlying select element.
+    function syncToSelect() {
+      for (var i = 0; i < selectElement.options.length; i++) {
+        var optionElem = selectElement.options[i];
+        var found = selectedTags.find(function(tag) {
+          return tag.id === optionElem.value;
+        });
+        optionElem.selected = !!found;
+      }
+      if (required) {
+        tagInput.required = selectedTags.length ? false : true;
+      } else {
+        tagInput.required = false;
+      }
+    }
+  
+    // Initialize the widget.
+    init();
+    renderSelectedTags();
+    syncToSelect();
+  
+    // Public API: Return an object exposing only public methods.
+    return {
+      selectAll: function() {
+        for (var i = 0; i < optionsData.length; i++) {
+          if (selectedTags.length >= maxSelection) break;
+          var opt = optionsData[i];
+          if (!selectedTags.find(function(tag) { return tag.id === opt.id; })) {
+            selectedTags.push({ id: opt.id, label: opt.label });
+          }
+        }
+        tagInput.value = '';
+        filteredOptions = optionsData.slice();
+        highlightedIndex = -1;
+        renderSelectedTags();
+        renderDropdown();
+        syncToSelect();
+        onChange(selectedTags);
+      },
+      clearAll: function() {
+        selectedTags = [];
+        renderSelectedTags();
+        renderDropdown();
+        syncToSelect();
+        onChange(selectedTags);
+      },
+      getSelectedTags: function() {
+        return selectedTags;
+      }
+    };
+  }   
